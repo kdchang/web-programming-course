ca7c6be8f73031bc79053d0b670263fccd0ef5ee
kdchang <kdchang.cc@gmail.com>
2014-09-30 18:43:35 +0800

    add php intro

---
+++ examples/Ch01/second.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php $color='red'; ?>
+	<h1>PHP is Rock</h1>
+	<h1 style="color:<?php echo $color;?>">PHP is Rock</h1>
+</body>
+</html>
