a6456baaa725a276becc5eee6b2e6e5cc9ae30ee
kdchang <kdchang.cc@gmail.com>
2014-11-04 18:36:48 +0800

    add F2E Sample

---
+++ examples/F2E/JS/main.js
@@ -0,0 +1,8 @@
+var result = 100;
+function add() {
+	var x = document.getElementById('x').value;
+	var y = document.getElementById('y').value;	
+	document.getElementById('result').innerHTML = "<h1>result</h1>";
+}
+
+
